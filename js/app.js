9a2d1f2f2d2249b5d57ae05bed025ea7b979ca6c
Lisa-1112 <blackman_lisa@yahoo.com>
2024-01-25 12:30:10 -0800

    leaveamessege

--- js/app.js
+++ js/app.js
@@ -1,58 +1,76 @@
-let question1 = prompt('Do I like plants?');
-let answer1 = 'yes';
+// let question1 = prompt('Do I like plants?');
+// let answer1 = 'yes';
 
-if (question1 === answer1) {
-    console.log ('your-correct');
-    alert('your-correct')
-} else {
-    console.log ('your-wrong');
-    alert('Your-wrong');
-}
+// if (question1 === answer1) {
+//     console.log ('your-correct');
+//     alert('your-correct')
+// } else {
+//     console.log ('your-wrong');
+//     alert('Your-wrong');
+// }
 
-let question2 = prompt('Do I like rainy weather?');
-let answer2 = 'yes';
+// let question2 = prompt('Do I like rainy weather?');
+// let answer2 = 'yes';
 
-if (question2 === answer2) {
-    console.log ('your-correct');
-    alert('your-correct');
-} else {
-    console.log ('your-wrong');
-    alert('your-wrong')
-}
+// if (question2 === answer2) {
+//     console.log ('your-correct');
+//     alert('your-correct');
+// } else {
+//     console.log ('your-wrong');
+//     alert('your-wrong')
+// }
 
-let question3 = prompt('Do I like mac and cheese?');
-let answer3 = 'yes';
+// let question3 = prompt('Do I like mac and cheese?');
+// let answer3 = 'yes';
 
-if (question3 === answer3) { 
-    console.log ('your-correct');
-    alert('your-correct')
-} else {
-    console.log ('your-wrong');
-    alert('your-wrong')
-}
+// if (question3 === answer3) { 
+//     console.log ('your-correct');
+//     alert('your-correct')
+// } else {
+//     console.log ('your-wrong');
+//     alert('your-wrong')
+// }
 
     
-let question4 = prompt('Do I like snow boarding?');
-let answer4 = 'yes';
+// let question4 = prompt('Do I like snow boarding?');
+// let answer4 = 'yes';
 
-if (question4 === answer4) {
-    console.log ('your-correct');
-    alert('your-correct')
-} else {
-    console.log ('your-wrong');
-    alert('your-wrong')
-}
+// if (question4 === answer4) {
+//     console.log ('your-correct');
+//     alert('your-correct')
+// } else {
+//     console.log ('your-wrong');
+//     alert('your-wrong')
+// }
 
 
-let question5 = prompt('Do I like going to the movies');
-let answer5 = 'yes';
+// let question5 = prompt('Do I like going to the movies');
+// let answer5 = 'yes';
 
-if (question5 === answer5) {
-    console.log ('your-correct');
-    alert('your-correct')   
-} else {
-    console.log ('your-wrong');
-    alert('your-wrong')
+// if (question5 === answer5) {
+//     console.log ('your-correct');
+//     alert('your-correct')   
+// } else {
+//     console.log ('your-wrong')
+//     alert('your-wrong')
+// }
+
+
+
+// This is our for loop
+for (let i = 0;i < 5; i++) {
+    document.write('Five tries!');
+    let question6 = prompt('guess my number');
+    let answer6 = '1';
+    
+    if (question6 === answer6) {
+        console.log ('your-correct');
+        alert('your-correct')
+    } else {
+        console.log ('try again wrong!')
+        alert('try again wrong')
+
+    }
 }
 
 
